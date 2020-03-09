a0447bc7e3afd8337f8e59093ff1e8b714b2c5e3
Zack <zack4@live.ca>
2020-03-09 11:11:48 -0400

    question 4

--- category-atelier.php
+++ category-atelier.php
@@ -1,13 +1,13 @@
 <?php
 
 
-get_header();
+
 ?>
 <div id="primary" class="content-area">
 <main id="main" class="site-main">
 <?php
 
-
+get_header();
 while ( have_posts() ) :
     the_post();
 //get_template_part( 'template-parts/content', 'page' );
@@ -16,11 +16,13 @@ while ( have_posts() ) :
 
 endwhile; // End of the loop.
 
+
+
+
 $args = array(
     "category_name" => "atelier",
     "posts_per_page" => 16,
-    "orderby" => "title",
-    "order" => "ASC"
+   
 
 );
 
@@ -30,19 +32,25 @@ $query1 = new WP_Query($args);
 //Affichage des Événements
 echo '<h2>' .category_description(get_category_by_slug('Atelier')) . '</h2>';
 
-
+    echo "<div id='grilleAteliers'>";
 
    while ( $query1->have_posts()) :
    $query1->the_post();
 
-   echo "<p style=color:grey>" . get_the_title() . " _____ ";
-   echo "<span style=color:red>" . get_post_field('post_name') . "</span>";
-   echo  "<span style=color:blue> _____ " . get_the_author_meta('display_name', $post->post_author) . "</p></span>";
+    $heure = (int)get_post_field('post_name');
+
+    $gridarea = $heure;
 
 
+
+    echo "<div>";
+   echo "<p style='grid-area: ".$gridarea."' > <span style=color:grey>" . get_the_title() . " _____ </span></p>";
+   echo "<p><span style=color:red>" . get_post_field('post_name') . "</span></p>";
+   echo  "<p><span style=color:blue> _____ " . get_the_author_meta('display_name', $post->post_author) . "</span></p>";
+    echo "</div>";
 
 endwhile;
  
-
+echo "</div>";
 
 ?>
